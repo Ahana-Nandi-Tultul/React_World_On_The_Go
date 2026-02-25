d8a721e27fe7c76d728036772a506c17fa4a00a6
Ahana Nandi <ahananondi6@gmail.com>
2026-02-25 20:46:33 +0600

    show countries

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css'
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([])
+    const handleVisitedCountries = () => {
+        alert("clieck handleVisitedCountries");
+    }
+    const countriesObj = use(countriesPromise);
+    const countries = countriesObj.countries;
+
+
+    // console.log(countries);
     return (
         <div>
-            <h1>In the world....</h1>
+            <h1>In the world: {countries.length}</h1>
+            <h3>Total Country Visited: </h3>
+            <div className='countries'>
+                {
+                    countries.map(country => <Country key={country?.cca3?.cca3}
+                        country={country}
+                        handleVisitedCountries = {handleVisitedCountries}></Country>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './Country.css';
+
+const Country = ({country, handleVisitedCountries}) => {
+    // console.log(country?.area?.area);
+    const [visited, setVisited] = useState(false)
+    const handleVisited = (country) =>{
+        console.log(country)
+        setVisited(!visited);
+        handleVisitedCountries();
+    }
+
+    return (
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country?.flags?.flags?.png} alt={country?.flags?.flags?.alt} srcset="" />
+            <h2>Name:{country?.name?.common}</h2>
+            <p>Population: {country?.population?.population}</p>
+            <p>Area:{country?.area?.area} { country?.area?.area > 300000 ? "Big Country": 
+                "Small Country"} </p>
+            <button onClick={()=>handleVisited(country)}>{visited? "Visited" : "Not Visited"}</button>
+        </div>
+    );
+};
+
+export default Country;
